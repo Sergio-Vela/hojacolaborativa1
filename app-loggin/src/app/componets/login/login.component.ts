a6b76292d75c0f8f442f40911a56254a93f0993f
Luis Castillo <alanmira68@gmail.com>
2026-02-07 18:39:07 -0600

    [UPDATE] login component

--- app-loggin/src/app/componets/login/login.component.ts
+++ app-loggin/src/app/componets/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Login } from 'src/app/interfaces/loginDto';
-import { LoginService } from 'src/app/services/login/login.service';
+import { Login } from '../../interfaces/loginDto';
+import { LoginService } from '../../services/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -15,21 +15,38 @@ export class LoginComponent {
   password: string = '';
 
   login() {
-    console.log('Usuario:', this.username);
-    console.log('Password:', this.password);
-
-    if (this.username !== '' && this.password !== '') {
-      const objectRequest: Login = {
-        username: this.username,
-        password: this.password 
-      };
-
-      this.loginService.doLogin(objectRequest).subscribe(entry => {
-        if (entry) {
-          console.log("login exitoso");
-        }
-      });
+    //espacios en blanco
+    if (this.username.trim() === '' || this.password.trim() === '') {
+      console.log("Usuario y contraseña son obligatorios");
+      return;
     }
 
+    //longitun del usuario minima
+    if (this.username.length < 4){
+      console.log("El nombre de usuario debe tener al menos 4 caracteres");
+      return;
+    }
+
+    //longitud minima de la contraseña
+    if (this.password.length > 6){
+      console.log("La contraseña no puede tener más de 6 caracteres");
+      return;
+    }
+
+    const objectRequest: Login = {
+      username: this.username,
+      password: this.password,
+    };
+
+    this.loginService.doLogin(objectRequest).subscribe({
+      
+      next: (entry) => {
+        console.log('Login exitoso:');
+      },
+      
+      error: (err) => {
+        console.error('Credenciales incorrectas o error del servidor:', err);
+      },
+    });
   }
 }
